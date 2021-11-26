e689237ac6e9fc877f15353d8fadf5ffbdb15519
duvalax <axel.duval74@gmail.com>
2021-11-26 21:00:06 +0100

    Put alerts component in protected routes only

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,5 +1,4 @@
 import { Container } from "@mui/material";
-import Alerts from "../components/Alerts";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import { DefaultLayoutContainer } from "./style";
@@ -8,7 +7,6 @@ const DefaultLayout = (props: React.PropsWithChildren<{}>) => (
     <DefaultLayoutContainer>
         <Navbar />
         <Container maxWidth="md" sx={{ minHeight: 'calc(100vh - 203px)' }}>
-            <Alerts />
             {props.children}
         </Container>
         <Footer />
--- src/router/protected.routes.tsx
+++ src/router/protected.routes.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
+import Alerts from "../components/Alerts";
 import Account from "../pages/Account";
 import Charts from "../pages/Charts";
 import Home from "../pages/Home";
@@ -8,15 +9,18 @@ import Testing from "../pages/Testing";
 import Vaccines from "../pages/Vaccines";
 
 const ProtectedRoutes = () => (
-    <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/news" exact component={News} />
-        <Route path="/charts" exact component={Charts} />
-        <Route path="/account" exact component={Account} />
-        <Route path="/vaccines" exact component={Vaccines} />
-        <Route path="/testing" exact component={Testing} />
-        <Redirect from="*" to="/" />
-    </Switch>
+    <>
+        <Alerts />
+        <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/news" exact component={News} />
+            <Route path="/charts" exact component={Charts} />
+            <Route path="/account" exact component={Account} />
+            <Route path="/vaccines" exact component={Vaccines} />
+            <Route path="/testing" exact component={Testing} />
+            <Redirect from="*" to="/" />
+        </Switch>
+    </>
 )
 
 export default React.memo(ProtectedRoutes);
